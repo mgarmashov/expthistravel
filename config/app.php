a9acc0e243516feb8e56632860f7ad877589d9a8
Mikhail Garmashov <mikhail.garmashov@gmail.com>
2018-10-22 02:16:46 +0500

    fix

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Experience this travel'),
 
     /*
     |--------------------------------------------------------------------------
