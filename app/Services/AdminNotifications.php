ed2430b4a2a74780690dc69bf3f85c2fdc60c020
Mikhail Garmashov <mikhail.garmashov@gmail.com>
2018-11-28 20:38:35 +0500

    bug

--- app/Services/AdminNotifications.php
+++ app/Services/AdminNotifications.php
@@ -10,10 +10,10 @@ class AdminNotifications
     {
         $admin = User::where('login', config('app.USER_NOTIFY'))->first();
 
-        if (isset($admin) && !count($admin)) {
+        if (!isset($admin)) {
             $admin = User::where('email', config('app.DEFAULT_EMAIL_NOTIFY'))->first();
         }
-        if (isset($admin) && !count($admin)) {
+        if (!isset($admin)) {
 
             $admin = new User;
             $admin->email = config('app.DEFAULT_EMAIL_NOTIFY');
