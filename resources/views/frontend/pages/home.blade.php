35203325ed38303bca2f80f74037f35297ecbdc1
Mikhail Garmashov <mikhail.garmashov@gmail.com>
2019-11-22 22:35:29 +0500

    Changed text on home page again

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -28,8 +28,7 @@
             <div class="pla1 p-home container">
                 <!-- TITLE & DESCRIPTION -->
                 <div class="spe-title spe-title-1">
-                    <h2>Tell us your <span>travel</span> preferences</h2>
-                    <h3>Answer a few questions to help us find your ideal trip</h3>
+                    <h2>How it <span>works</span></h2>
                 </div>
                 <div class="howItWorks-container">
                     <div class="col-md-3 col-sm-6">
@@ -37,8 +36,8 @@
                             <div class="number">1</div>
                             {{--<i class="fa fa-check"></i>--}}
                             <img src="{{ asset('images/icon-tick.png') }}" alt="">
-                            <h5>Take our travel profiler test </h5>
-                            <p>You tell us what you’re looking for from your trip with our quick and easy quiz</p>
+                            <h5>Tell us your travel preferences</h5>
+                            <p>Answer a few questions to help us find your ideal trip</p>
                         </div>
                     </div>
 
