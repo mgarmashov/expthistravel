39ea19be4efb866353bc708e86e431c8a079fcda
mgarmashov <mikhail.garmashov@gmail.com>
2019-11-29 00:15:21 +0500

    deleted month list in filter

--- resources/views/frontend/components/filter-form.blade.php
+++ resources/views/frontend/components/filter-form.blade.php
@@ -26,27 +26,11 @@
                 @endforeach
             </select>
         </div>
-        @php
-            $months = [
-                1 => 'January',
-                2 => 'February',
-                3 => 'March',
-                4 => 'April',
-                5 => 'May',
-                6 => 'June',
-                7 => 'July',
-                8 => 'August',
-                9 => 'September',
-                10 => 'October',
-                11 => 'November',
-                12 => 'December'
-            ];
-        @endphp
         <div class="col-sm-4">
             <label for="filter-month">Select month</label>
             <select id="filter-month" multiple name="month[]">
 {{--                <option value="all" {{ isset($filter['month']) && in_array('all', $filter['month']) ? 'selected' : '' }}>Any</option>--}}
-                @foreach($months as $key => $month)
+                @foreach(monthsList() as $key => $month)
                     <option value="{{ $key }}" {{ isset($filter['month']) && !in_array('all', $filter['month']) && in_array($key, $filter['month']) ? 'selected' : '' }}>{{ $month }}</option>
                 @endforeach
             </select>
