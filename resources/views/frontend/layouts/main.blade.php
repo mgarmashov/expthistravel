b117942fd0cc4cb4eb905e3a77abe3c9309b41d1
mgarmashov <mikhail.garmashov@gmail.com>
2019-11-30 14:52:16 +0500

    google tag in env changing

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -8,8 +8,8 @@
     {{-- Encrypted CSRF token for Laravel, in order for Ajax requests to work --}}
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    @if(env('google-site-verification'))
-        <meta name="google-site-verification" content="{{ env('google-site-verification') }}" />
+    @if(env('googleSiteVerification'))
+        <meta name="googleSiteVerification" content="{{ env('googleSiteVerification') }}" />
     @endif
     @stack('seo')
     <meta property="og:type" content="website" />
