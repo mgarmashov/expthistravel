78961e122c7287c51057cf9a566b6f57ab07032c
mgarmashov <mikhail.garmashov@gmail.com>
2019-12-25 22:42:31 +0500

    bugs

--- resources/views/frontend/components/section-popularItineraries.blade.php
+++ resources/views/frontend/components/section-popularItineraries.blade.php
@@ -8,7 +8,7 @@
         <div class="col-md-6 col-sm-6 col-xs-12 place">
             <div class="col-md-6 col-sm-12 col-xs-12"> <img src="{{ asset(cropImage($itinerary->image_main, 250, 230)) }}" alt="" /> </div>
             <div class="col-md-6 col-sm-12 col-xs-12">
-                <a href="{{ route('product', ['id' => $itinerary->slug]) }}"><h3>{{ $itinerary->name }}</h3></a>
+                <a href="{{ route('itinerary', ['id' => $itinerary->slug]) }}"><h3>{{ $itinerary->name }}</h3></a>
                 <p>{{ $itinerary->description_short }}</p>
                 <div class="featur">
                     <h4>{{ $itinerary->countries()->first()->name ?? '' }}</h4>
@@ -19,7 +19,7 @@
                 <div class="p2_book">
                     <ul>
                         {{--<li><a href="#" class="link-btn btn-book-product" data-product="{{$itinerary->id}}">Add to my trip</a> </li>--}}
-                        <li><a href="{{ route('product', ['id' => $itinerary->slug]) }}" class="link-btn">Find out more</a> </li>
+                        <li><a href="{{ route('itinerary', ['id' => $itinerary->slug]) }}" class="link-btn">Find out more</a> </li>
                     </ul>
                 </div>
             </div>
