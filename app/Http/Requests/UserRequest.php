3c3e21207814c3a10c2e5a04691fe055f5f60dd9
mgarmashov <mikhail.garmashov@gmail.com>
2020-02-04 23:50:41 +0500

    admin. set users Name and Email not required

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -26,8 +26,8 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-             'name' => 'required|min:2|max:255',
-             'login' => 'required|min:2|max:255',
+             'name' => 'min:2|max:255',
+             'login' => 'min:2|max:255',
              'email' => 'required|email',
              'role' => 'required'
         ];
